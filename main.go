47c1bb7d9fe1bedbb20c5fadebf089895ad08da4
sanity32 <qwe123qqqq@gmail.com>
2024-04-15 01:09:20 +0300

    1

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/sanity32/v17localresident"
+)
+
+var port int = v17localresident.DEFAULT_RPC_SERVER_PORT
+var host string = "localhost"
+
+func parseFlags() {
+	flag.IntVar(&port, "port", port, "RPC server port")
+	flag.StringVar(&host, "host", host, "RPC server host")
+	flag.Parse()
+}
+
+func main() {
+	parseFlags()
+	addr := fmt.Sprintf("%v:%v", host, port)
+	ch, err := v17localresident.NewServer(addr).Setup().Run()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	log.Println("Server is up and running @", addr)
+	<-ch
+}
